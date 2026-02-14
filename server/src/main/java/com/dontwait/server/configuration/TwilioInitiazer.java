7b3fa995be56bf716fd5e9eafae42bade34f3c92
dont-wait <nguyentansang15042005@gmail.com>
2026-02-14 14:06:32 +0700

    feat: initializer config for twilio

---
+++ server/src/main/java/com/dontwait/server/configuration/TwilioInitiazer.java
@@ -0,0 +1,25 @@
+package com.dontwait.server.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import com.twilio.Twilio;
+
+import lombok.experimental.FieldDefaults;
+
+@Configuration
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
+public class TwilioInitiazer {
+    TwilioConfig twilioConfig;
+    static Logger LOGGER = LoggerFactory.getLogger(TwilioInitiazer.class); 
+
+    @Autowired
+    public TwilioInitiazer(TwilioConfig twilioConfig) {
+        this.twilioConfig = twilioConfig;
+        Twilio.init(twilioConfig.getAccountSid(), twilioConfig.getAuthToken());
+        LOGGER.info("Twilio initialized with account SID: {}", twilioConfig.getAccountSid());
+    }
+
+}
